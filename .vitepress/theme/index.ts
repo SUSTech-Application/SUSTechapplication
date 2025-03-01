1eb577ee8077c28115f4be01269fbc822f281f1e
Samuel Huang <hi@sghuang.com>
2025-02-28 23:13:19 -0500

    use netlify redirect

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,21 +6,7 @@ import DefaultTheme from "vitepress/theme";
 
 import "./style.css";
 
-/** redirect hash based routing URL to history based routing */
-const redirect = (router: Router) => {
-  if (typeof window !== "undefined") {
-    watchEffect(() => {
-      if (window.location.hash.startsWith("#/")) {
-        const path = window.location.hash.slice(2);
-        router.go(`/${path}`);
-      }
-    });
-  }
-};
-
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app, router, siteData }) {
-    redirect(router);
-  },
+  enhanceApp({ app, router, siteData }) {},
 } satisfies Theme;
