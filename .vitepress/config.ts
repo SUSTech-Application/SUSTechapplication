e91b3c3c2aafa0819a3075b118b982cfad20b69c
Samuel Huang <hi@sghuang.com>
2025-03-17 09:36:47 -0400

    feat: Google Analytics

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -24,4 +24,21 @@ export default defineConfig({
       },
     ],
   },
+  head: [
+    [
+      "script",
+      {
+        async: "",
+        src: "https://www.googletagmanager.com/gtag/js?id=G-VVQKMMQ3VE",
+      },
+    ],
+    [
+      "script",
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); };
+      gtag("js", new Date());
+      gtag("config", "G-VVQKMMQ3VE");`,
+    ],
+  ],
 });
