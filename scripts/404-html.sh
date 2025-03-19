cb4aecd21076b558b9028b163047ed518c07e6cf
Samuel Huang <hi@sghuang.com>
2025-03-18 22:54:29 -0400

    escape quotes in script

--- scripts/404-html.sh
+++ scripts/404-html.sh
@@ -2,10 +2,10 @@
 # injects a script in 404 to help redirect URL without .html extension
 # FIXME: this is a temporary workaround!
 
-SCRIPT='<script>
-if (!window.location.pathname.endsWith(".html") && !window.location.pathname.endsWith("/")) {
-  window.location.href = window.location.pathname + ".html" + window.location.search;
+SCRIPT="<script>
+if (!window.location.pathname.endsWith(\".html\") && !window.location.pathname.endsWith(\"/\")) {
+  window.location.href = window.location.pathname + \".html\" + window.location.search;
 }
-</script>'
+</script>"
 
 sed -i "s|</head>|$SCRIPT\n</head>|" .vitepress/dist/404.html
