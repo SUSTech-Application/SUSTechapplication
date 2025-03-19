71a5031e9288a9d5d30436efe31435f424a7182b
Samuel Huang <hi@sghuang.com>
2025-03-18 22:49:33 -0400

    create script

---
+++ scripts/404-html.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# injects a script in 404 to help redirect URL without .html extension
+# FIXME: this is a temporary workaround!
+
+SCRIPT='<script>
+if (!window.location.pathname.endsWith(".html") && !window.location.pathname.endsWith("/")) {
+  window.location.href = window.location.pathname + ".html" + window.location.search;
+}
+</script>'
+
+sed -i "s|</head>|$SCRIPT\n</head>|" .vitepress/dist/404.html
