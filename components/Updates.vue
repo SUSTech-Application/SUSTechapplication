9819af9039138273fb7b8182d9075a6ddbb97cc1
Samuel Huang <hi@sghuang.com>
2025-03-21 16:41:26 -0400

    replace updates with components

---
+++ components/Updates.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { data as posts } from "../utils/posts.data.ts";
+console.log(posts);
+</script>
+
+<template>
+
+  <ul>
+
+    <li v-for="post of posts">
+       [{{ post.date?.slice(0, 10) ?? "No date" }}] - <a :href="post.url">{{
+        post.title ?? "No title"
+      }}</a
+      >,
+    </li>
+
+  </ul>
+
+</template>
+
