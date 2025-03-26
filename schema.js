2f2b479ed6e41e179d3a94b145b809f248357cd9
Samuel Huang <hi@sghuang.com>
2025-03-25 23:20:17 -0400

    create js schema

---
+++ schema.js
@@ -0,0 +1,54 @@
+let schema = {
+  type: "object",
+  properties: {
+    department: { type: "string" },
+    degree: { type: "string" },
+    employer: { type: "string" },
+    program: { type: "string" },
+    region: { type: "object" },
+    title: { type: "string" },
+    type: {
+      type: "string",
+      enum: ["grad", "abroad", "job", "recruit", "experience"],
+    },
+    university: { type: "string" },
+    year: { type: "number" },
+  },
+  required: ["title", "type"],
+};
+
+const gradSchema = {
+  properties: { type: { const: "grad" } },
+  required: ["department", "program", "region", "university", "year"],
+  not: { required: ["employer"] },
+};
+
+const abroadSchema = {
+  properties: { type: { const: "abroad" } },
+  required: ["region", "university"],
+  not: { required: ["degree", "employer"] },
+};
+
+const jobSchema = {
+  properties: { type: { const: "job" }, employer: { type: "string" } },
+  required: ["department", "employer", "region", "year"],
+  not: { required: ["degree", "program", "university"] },
+};
+
+const recruitSchema = {
+  properties: { type: { const: "recruit" } },
+  required: ["program", "region", "university"],
+  not: { required: ["employer"] },
+};
+
+const experienceSchema = { properties: { type: { const: "experience" } } };
+
+schema.oneOf = [
+  gradSchema,
+  abroadSchema,
+  jobSchema,
+  recruitSchema,
+  experienceSchema,
+];
+
+export default schema;
