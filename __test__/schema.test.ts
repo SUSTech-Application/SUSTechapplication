eb9eff15f97a91d1d7ea8ef8152cca4999839fe2
Samuel Huang <hi@sghuang.com>
2025-03-26 20:32:11 -0400

    chore: add schema for front matter (#186)
    * create metadata.yaml
    
    * add comments into metadata.yaml
    
    * create js schema
    
    * pull metadata from yaml file
    
    * use objects instead of arrays for metadata
    
    * use imported metadata for schema
    
    * abstract options parsing
    
    * rename to schema.ts
    
    * fixing type problems
    
    * add test cases for schema
    
    * empty schema to suppress vscode warning
    
    * add schema for date
    
    * extra constraints for date, region, year
    
    * extra constraints for year
    
    * add author
    
    * use an actual schema for metadata
    
    * add a test case
    
    * fix a problem in date regex

---
+++ __test__/schema.test.ts
@@ -0,0 +1,27 @@
+import Ajv from "ajv/dist/2020";
+import { expect, test } from "vitest";
+
+import schema from "../schema";
+
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
+
+test("frontmatter schema should be valid", () => {
+  expect(validate).toBeTruthy();
+});
+
+test("schema should work as intended (positive)", () => {
+  const data = {
+    author: "Samuel Huang",
+    type: "grad",
+    year: 19,
+    university: "nd",
+    region: "us",
+    department: "sme",
+    title: "Notre Dame",
+    degree: "master",
+    date: "2025-03-26",
+    program: "Computer Science and Engineering",
+  };
+  expect(validate(data)).toBe(true);
+});
---
+++ schema.ts
@@ -0,0 +1,72 @@
+import { readFileSync } from "fs";
+import { parse } from "yaml";
+
+const metadata = parse(readFileSync("./metadata.yaml", "utf-8"));
+const options = Object.fromEntries(
+  Object.keys(metadata).map((k) => [k, Object.keys(metadata[k])]),
+);
+
+let schema = {
+  type: "object",
+  properties: {
+    author: { type: "string" },
+    date: { type: "string", pattern: "^\\d{4}-\\d{2}-\\d{2}$" },
+    department: { type: "string", enum: options.department },
+    degree: { type: "string", enum: options.degree },
+    employer: { type: "string" },
+    program: { type: "string" },
+    region: {
+      // either a region, or a list of regions
+      oneOf: [
+        { type: "string", enum: options.region },
+        {
+          type: "array",
+          minItems: 2,
+          items: { type: "string", enum: options.region },
+        },
+      ],
+    },
+    title: { type: "string" },
+    type: { type: "string", enum: options.type },
+    university: { type: "string", enum: options.university },
+    year: { type: "integer", minimum: 11, maximum: 99 },
+  },
+  required: ["author", "date", "title", "type"],
+};
+
+const gradSchema = {
+  properties: { type: { const: "grad" } },
+  required: ["department", "program", "region", "university", "year"],
+  not: { required: ["employer"] },
+};
+
+const abroadSchema = {
+  properties: { type: { const: "abroad" } },
+  required: ["region", "university"],
+  not: { required: ["degree", "employer"] },
+};
+
+const jobSchema = {
+  properties: { type: { const: "job" }, employer: { type: "string" } },
+  required: ["department", "employer", "region", "year"],
+  not: { required: ["degree", "program", "university"] },
+};
+
+const recruitSchema = {
+  properties: { type: { const: "recruit" } },
+  required: ["program", "region", "university"],
+  not: { required: ["employer"] },
+};
+
+const experienceSchema = { properties: { type: { const: "experience" } } };
+
+// @ts-expect-error oneOf doesn't exist on schema
+schema.oneOf = [
+  gradSchema,
+  abroadSchema,
+  jobSchema,
+  recruitSchema,
+  experienceSchema,
+];
+
+export default schema;
