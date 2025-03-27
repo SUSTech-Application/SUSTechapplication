12680474b1a1929f1d3944c42c3ca4231b3cf6d0
Samuel Huang <hi@sghuang.com>
2025-03-27 15:48:25 -0400

    fix file include/exclude

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,7 @@ import vue from "eslint-plugin-vue";
 import ts from "typescript-eslint";
 
 export default ts.config(
+  { ignores: [".vitepress/cache/**/*"] },
   js.configs.recommended,
   ts.configs.strictTypeChecked,
   ts.configs.stylisticTypeChecked,
