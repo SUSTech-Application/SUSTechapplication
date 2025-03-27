28114c406775b4ebc3921785e53da4cddaf5731a
Samuel Huang <hi@sghuang.com>
2025-03-26 20:28:01 -0400

    add a test case

--- __test__/schema.test.ts
+++ __test__/schema.test.ts
@@ -1,9 +1,27 @@
-import Ajv from "ajv";
+import Ajv from "ajv/dist/2020";
 import { expect, test } from "vitest";
 
 import schema from "../schema";
 
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
+
 test("frontmatter schema should be valid", () => {
-  const ajv = new Ajv();
-  expect(ajv.compile(schema)).toBeTruthy();
+  expect(validate).toBeTruthy();
+});
+
+test("schema should work as intended (positive)", () => {
+  const data = {
+    author: "Samuel Huang",
+    type: "grad",
+    year: 19,
+    university: "nd",
+    region: "us",
+    department: "sme",
+    title: "Notre Dame",
+    degree: "master",
+    date: "2025-03-26",
+    program: "Computer Science and Engineering",
+  };
+  expect(validate(data)).toBe(true);
 });
