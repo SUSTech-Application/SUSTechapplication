aa1b9c22a09347d14ff8dd2731ee1fce962267ff
Samuel Huang <hi@sghuang.com>
2025-03-26 20:28:17 -0400

    fix a problem in date regex

--- schema.ts
+++ schema.ts
@@ -10,7 +10,7 @@ let schema = {
   type: "object",
   properties: {
     author: { type: "string" },
-    date: { type: "string", pattern: "^\d{4}-\d{2}-\d{2}$" },
+    date: { type: "string", pattern: "^\\d{4}-\\d{2}-\\d{2}$" },
     department: { type: "string", enum: options.department },
     degree: { type: "string", enum: options.degree },
     employer: { type: "string" },
