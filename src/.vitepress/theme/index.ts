5f30d318107be92aff80ec4aa29dded9cd4ecc21
Samuel Huang <hi@sghua.ng>
2025-03-28 17:13:00 -0400

    move sidebar around

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,6 +1,4 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
-
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 
@@ -11,11 +9,6 @@ import "./style.css";
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
-  },
   enhanceApp({ app, router }) {
     /* redirect legacy hash path */
     router.onAfterRouteChange = async (to) => {
