a72fe0e5b66858885273bdf881fb92d770a53458
Samuel Huang <hi@sghuang.com>
2025-03-27 22:41:29 -0400

    minor fix to schema.ts

--- src/schema.ts
+++ src/schema.ts
@@ -16,8 +16,8 @@ const schema = {
   properties: {
     author: { type: "string" },
     date: { type: "string", pattern: "^\\d{4}-\\d{2}-\\d{2}$" },
-    department: { type: "string", enum: options.department },
     degree: { type: "string", enum: options.degree },
+    department: { type: "string", enum: options.department },
     employer: { type: "string" },
     program: { type: "string" },
     region: {
