80690fbf9fe502cf9f0232304467b1e29c529a33
Samuel Huang <hi@sghua.ng>
2025-03-29 19:35:15 -0400

    extract first heading as title

--- __test__/posts.test.ts
+++ __test__/posts.test.ts
@@ -1,11 +1,16 @@
 import { expect, test } from "vitest";
 
 import posts from "../src/posts";
-import { getFrontmatter } from "../src/utils";
+import { getFrontmatter, getH1 } from "../src/utils";
 
 test("frontmatter is loaded correctly", () => {
   const frontmatter = getFrontmatter("__test__/test.md");
   expect(frontmatter).toStrictEqual({ key1: "hello world", key2: 42 });
+});
+
+test("first heading is read correctly", () => {
+  const h1 = getH1("__test__/test.md");
+  expect(h1).toStrictEqual("Hello World");
 });
 
 test("cms loads posts correctly", () => {
--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,10 @@ export function getFrontmatter(path: string) {
   const content = fs.readFileSync(path, "utf8");
   return matter(content).data;
 }
+
+/** Get the first heading */
+export function getH1(path: string) {
+  const content = fs.readFileSync(path, "utf-8");
+  const match = /^# (.+)$/m.exec(content);
+  return match?.at(1)?.trim() ?? "";
+}
