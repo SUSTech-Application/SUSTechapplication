c17d29f526df051e58430b3c755d0556ec0361ee
Samuel Huang <hi@sghua.ng>
2025-03-29 22:23:16 -0400

    paste in old sidebar

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -38,6 +38,68 @@ const gradSchool: DefaultTheme.SidebarItem = {
 
 */
 
+const gradSchool: DefaultTheme.SidebarItem = {
+  text: "个人申请总结",
+  collapsed: false,
+  items: [
+    {
+      items: [
+        {
+          text: "计算机科学与工程系 (21)",
+          link: "grad-application/computer-science-and-engineering",
+        },
+        {
+          text: "生物医学工程系 (11)",
+          link: "grad-application/biomedical-engineering",
+        },
+        { text: "物理系 (7)", link: "grad-application/physics" },
+        { text: "生物系 (6)", link: "grad-application/biology" },
+        {
+          text: "力学与航空航天工程系 (8)",
+          link: "grad-application/mechanics-and-aerospace-engineering",
+        },
+        { text: "化学系 (6)", link: "grad-application/chemistry" },
+        {
+          text: "地球与空间科学系 (1)",
+          link: "grad-application/earth-and-space-science",
+        },
+        { text: "数学与统计系 (13)", link: "grad-application/math" },
+        {
+          text: "机械与能源工程系 (4)",
+          link: "grad-application/mechanical-and-energy-engineering",
+        },
+        {
+          text: "材料科学与工程系 (9)",
+          link: "grad-application/materials-science-and-engineering",
+        },
+        {
+          text: "海洋科学与工程系 (2)",
+          link: "grad-application/marine-science-and-engineering",
+        },
+        {
+          text: "环境科学与工程学院 (4)",
+          link: "grad-application/environmental-science-and-engineering",
+        },
+        {
+          text: "电子与电气工程系 (10)",
+          link: "grad-application/electronic-and-electrical-engineering",
+        },
+        { text: "金融系 (19)", link: "grad-application/finance" },
+        { text: "医学院 (3)", link: "grad-application/medicine" },
+        {
+          text: "深港微电子学院 (10)",
+          link: "grad-application/microelectronics",
+        },
+        {
+          text: "信息系统与管理工程系 (1)",
+          link: "grad-application/information-systems-and-management-engineering",
+        },
+        { text: "系统设计与智能制造学院 (1)", link: "grad-application/sdim" },
+      ],
+    },
+  ],
+};
+
 const studyAbroad: DefaultTheme.SidebarItem = {
   text: "海外交流",
   collapsed: false,
