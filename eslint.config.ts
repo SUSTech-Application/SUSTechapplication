82601e475e17e0908bfa1590db7dff3b79bf082d
Samuel Huang <hi@sgh.ng>
2025-04-06 21:46:39 -0400

    chore: improve eslint config for vue (#227)
    * extend from vue base tsconfig
    
    * improve import sorting for vue and vite
    
    * improve eslint for vue and ts
    
    * add prettier eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,25 @@
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import vue from "eslint-plugin-vue";
 import ts from "typescript-eslint";
 
 export default ts.config(
-  {
-    ignores: [
-      "cache",
-      "dist",
-      // FIXME: remove this once we no longer need the bracket fix
-      "**/*.js",
-    ],
-  },
   js.configs.recommended,
   ts.configs.strictTypeChecked,
   ts.configs.stylisticTypeChecked,
   vue.configs["flat/recommended"],
-  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    // FIXME: remove this once we no longer need the bracket fix
+    ignores: ["**/*.js"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        extraFileExtensions: ["vue"],
+        parser: ts.parser,
+        projectService: true,
+      },
+    },
+  },
+  prettier,
 );
