b3fd2cfc8f80d0f3d5d3f3297d87c8a2ef45c142
Samuel Huang <hi@sgh.ng>
2025-04-12 12:34:48 -0400

    most basic PostList.vue implementation

---
+++ src/components/PostList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Post } from "../posts";
+
+defineProps<{
+  posts: Post[];
+}>();
+</script>
+
+<template>
+  <ul>
+    <li v-for="post in posts" :key="post.url">
+      {{ post.title }}
+    </li>
+  </ul>
+</template>
