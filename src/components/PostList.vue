64e674618acce9ea034b724552ae3dd3ab404ca5
Samuel Huang <hi@sgh.ng>
2025-04-14 19:55:33 -0400

    update PostList to include type

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import METADATA from "../metadata";
 import type { Page } from "../pages.data";
 
 defineProps<{ posts: Page[] }>();
@@ -8,6 +9,7 @@ defineProps<{ posts: Page[] }>();
   <ul>
     <li v-for="post in posts" :key="post.url">
       {{ post.metadata.date }} -
+      {{ `[${METADATA.type[post.metadata.type]}]` }} -
       <a :href="post.url">{{ post.title ?? "无标题" }}</a>
     </li>
   </ul>
