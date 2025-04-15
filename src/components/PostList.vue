cbe931e9ef5a723e1694d952756c1454eec47394
Samuel Huang <hi@sgh.ng>
2025-04-14 20:15:51 -0400

    feat: updates with component (#174)
    * update PostList to include type
    
    * replace the update list in updates.md
    
    * suppress typing temporarily

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import METADATA from "../metadata";
 import type { Page } from "../pages.data";
 
 defineProps<{ posts: Page[] }>();
@@ -8,6 +9,9 @@ defineProps<{ posts: Page[] }>();
   <ul>
     <li v-for="post in posts" :key="post.url">
       {{ post.metadata.date }} -
+      <!-- FIXME: need to validate metadata type -->
+      <!-- @vue-expect-error need to type this -->
+      {{ `[${METADATA.type[post.metadata.type]}]` }} -
       <a :href="post.url">{{ post.title ?? "无标题" }}</a>
     </li>
   </ul>
