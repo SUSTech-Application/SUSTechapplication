c75d6ae3316d5292e0729f33d093e6d40e15dd93
Samuel Huang <hi@sgh.ng>
2025-05-19 20:06:18 -0400

    remove unneeded transform config

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -57,9 +57,6 @@ export default {
       gtag("config", "G-VVQKMMQ3VE");`,
     ],
   ],
-
-  vite,
-
   sitemap: {
     hostname: "https://sustech-application.com",
     exclude: [
@@ -68,14 +65,6 @@ export default {
       "/content/region/[key].html",
       "/content/department/[key].html",
     ],
-    transformItems: (items) => {
-      // 添加新项目或修改/筛选现有选项
-      items.push({
-        url: "/extra-page",
-        changefreq: "monthly",
-        priority: 0.8,
-      });
-      return items;
-    },
   },
+  vite,
 } as UserConfig;
